a0c78ed836e1f93189cdfb352364eea1cd4db55c
gilbertlcsndle <gilbert.lacasandile@gmail.com>
2017-11-20 16:44:58 +0800

    Finish static pages

--- Gemfile
+++ Gemfile
@@ -14,11 +14,13 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
+gem 'pg', '~> 0.21.0'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'sqlite3'
   gem 'rspec-rails', '~> 3.7', '>= 3.7.1'
+  gem 'guard-rspec', '4.7.3'
+  gem 'spring-commands-rspec', '~> 1.0', '>= 1.0.4'
 end
 
 group :development do
@@ -30,10 +32,6 @@ end
 
 group :test do
   gem 'capybara', '~> 2.16'
-end
-
-group :production do
-  gem 'pg', '~> 0.21.0'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+describe "Static pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      get '/static_pages/home'
+      expect(response.body).to include('Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      get '/static_pages/home'
+      assert_select 'title', "#{base_title} | Home"
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      get '/static_pages/help'
+      expect(response.body).to include('Help')
+    end
+
+    it "should have the title 'Help'" do
+      get '/static_pages/help'
+      assert_select 'title', "#{base_title} | Help"
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      get '/static_pages/about'
+      expect(response.body).to include('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      get '/static_pages/about'
+      assert_select 'title', "#{base_title} | About Us"
+    end
+  end
+
+  describe 'Contact page' do
+
+    it "should have the content 'Contact'" do
+      get '/static_pages/contact'
+      expect(response.body).to include('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      get '/static_pages/contact'
+      assert_select 'title', "#{base_title} | Contact"
+    end
+  end
+end
