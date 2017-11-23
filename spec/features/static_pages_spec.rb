28c574e1979412fe91de3ef70e354f629df191c9
gilbertlcsndle <gilbert.lacasandile@gmail.com>
2017-11-23 16:15:51 +0800

    Finish sign in

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,23 +4,23 @@ describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
-  subject { response.body }
+  subject { page }
 
   shared_examples_for 'all static pages' do
-    it { assert_select 'h1', heading }
-    it { assert_select 'title', full_title(page_title) }
+    it { should have_selector('h1', heading) }
+    it { should have_title(page_title) }
   end
 
   describe "Home page" do
-    before { get root_path }
-    let(:heading) { /Sample App/ }
+    before { visit root_path }
+    let(:heading) { 'Sample App' }
     let(:page_title) { '' }
 
     it_should_behave_like 'all static pages'
   end
 
   describe "Help page" do
-    before { get help_path }
+    before { visit help_path }
     let(:heading) { 'Help' }
     let(:page_title) { 'Help' }
 
@@ -28,7 +28,7 @@ describe "Static pages" do
   end
 
   describe "About page" do
-    before { get about_path }
+    before { visit about_path }
     let(:heading) { 'About Us' }
     let(:page_title) { 'About Us' }
 
@@ -36,7 +36,7 @@ describe "Static pages" do
   end
 
   describe 'Contact page' do
-    before { get contact_path }
+    before { visit contact_path }
     let(:heading) { 'Contact' }
     let(:page_title) { 'Contact' }
 
--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -9,6 +9,21 @@ describe 'User pages' do
 
     subject { page }
 
+    describe "profile page" do
+      let(:user) { FactoryBot.create(:user) }
+      before { visit user_path(user) }
+
+      it { should have_content(user.name) }
+      it { should have_title(user.name) }
+    end
+
+    describe "signup page" do
+      before { visit signup_path }
+
+      it { should have_content('Sign up') }
+      it { should have_title('Sign up') }
+    end
+
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
@@ -38,6 +53,7 @@ describe 'User pages' do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
+        it { should have_link('Sign out') }
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
